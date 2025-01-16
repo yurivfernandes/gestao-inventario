03209454a27e164873ab26c7565f9c4608a5df94
Yuri Viana Fernandes <y.fernandes@vp6.com.br>
2025-01-16 15:56:21 -0300

    Ajustado o settings para aceitar a conexão com o banco de dados e ajustado o readme.md.

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -87,7 +87,18 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    }
+    },
+    "sql_server": {
+        "ENGINE": "mssql",
+        "NAME": "nome_do_banco",
+        "USER": "usuario",
+        "PASSWORD": "senha",
+        "HOST": "xxxx",
+        "PORT": "",
+        "OPTIONS": {
+            "driver": "ODBC Driver 17 for SQL Server",
+        },
+    },
 }
 
 
