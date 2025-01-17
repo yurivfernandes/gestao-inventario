1e836638cbe9affa2c0e4ec80502cb9ea36021c4
Yuri Viana Fernandes <y.fernandes@vp6.com.br>
2025-01-17 18:34:45 -0300

    Criado a página de gestão de incidentes e atualizado os filtros.

--- backend/incidentes/api/urls.py
+++ backend/incidentes/api/urls.py
@@ -1,5 +1,7 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from . import view
 
 urlpatterns = [
+    path("list/", view.IncidenteListView.as_view(), name="incidente-list"),
 ]
